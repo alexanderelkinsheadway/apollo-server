69b6dc51a2fa77a1d32001befd7313a89d393877
alexanderelkinsheadway <74934300+alexanderelkinsheadway@users.noreply.github.com>
2020-11-24 08:47:48 -0800

    Tried using Digital Ocean Example Digital Ocean has an example with User and Recipe. There is a few errors about Schema is not configured for mutations.

---
+++ models/index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
+// src/index.js
+
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const models = require("../models");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { models }
+});
+
+server
+  .listen()
+  .then(({ url }) => console.log("Server is running on localhost:4000"));
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
